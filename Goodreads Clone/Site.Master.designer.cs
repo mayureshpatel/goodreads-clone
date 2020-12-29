e9baddabf36772db06051bb8d03748dbe6377141
HungryTiger95 <mayureshpatel@yahoo.com>
2020-12-29 09:13:21 -0500

    Restructured header html and added dropdown for browse

--- Goodreads Clone/Site.Master.designer.cs
+++ Goodreads Clone/Site.Master.designer.cs
@@ -33,6 +33,24 @@ namespace Goodreads_Clone
         protected global::System.Web.UI.HtmlControls.HtmlInputText hiddenSearchTextBox;
 
         /// <summary>
+        /// BrowseListsContentPlaceholder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder BrowseListsContentPlaceholder;
+
+        /// <summary>
+        /// BrowseGenresContentPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder BrowseGenresContentPlaceHolder;
+
+        /// <summary>
         /// searchTextBox control.
         /// </summary>
         /// <remarks>
