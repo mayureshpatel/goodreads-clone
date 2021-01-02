4dc9c1781347fa4a84601c1c72bcb7c49194dcfd
HungryTiger95 <mayureshpatel@yahoo.com>
2021-01-02 13:29:07 -0500

    Fixed an issue with add new book styling

--- Goodreads Clone/Scripts/master-main.js
+++ Goodreads Clone/Scripts/master-main.js
@@ -75,13 +75,11 @@
     // Be able to click anywhere to hide the Browse drop down
     $("body").click(function (evt) {
         if (evt.target.id == "#BrowseDropDown") {
-            console.log("nope 1");
             return;
         }
             
         //For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
         if ($(evt.target).closest("#BrowseDropDown").length) {
-            console.log("nope 2");
             return;
         }
 
